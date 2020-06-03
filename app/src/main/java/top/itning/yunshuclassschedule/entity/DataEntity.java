0bbba290c5212e850078d831a793e3db264ce111
LH-L <153485605@qq.com>
2020-06-03 22:39:23 +0800

    version 1.1

---
+++ app/src/main/java/top/itning/yunshuclassschedule/entity/DataEntity.java
@@ -0,0 +1,37 @@
+package top.itning.yunshuclassschedule.entity;
+
+import java.util.List;
+
+import top.itning.yunshuclassschedule.common.App;
+import top.itning.yunshuclassschedule.util.DateUtils;
+
+/**
+ * 数据封装实体
+ *
+ * @author itning
+ */
+public class DataEntity {
+    private final List<ClassSchedule> classScheduleList;
+    private final List<String> timeList;
+
+    public DataEntity(App app) {
+        classScheduleList = app.getDaoSession().getClassScheduleDao().loadAll();
+        timeList = DateUtils.INSTANCE.getTimeList();
+    }
+
+    public List<ClassSchedule> getClassScheduleList() {
+        return classScheduleList;
+    }
+
+    public List<String> getTimeList() {
+        return timeList;
+    }
+
+    @Override
+    public String toString() {
+        return "DataEntity{" +
+                "classScheduleList=" + classScheduleList +
+                ", timeList=" + timeList +
+                '}';
+    }
+}
